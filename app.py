6f3328e608dd85f45675c371a1146f45452ab3f8
Vincius <vnpp856@gmail.com>
2025-05-03 15:38:15 -0300

    Validações do campo validade

--- app.py
+++ app.py
@@ -2,9 +2,21 @@
 O QUE FOI FEITO NESTE CÓDIGO:
 (Escreva aqui o que você fez no código para facilitar para o próximo entender)
 
-1. Formatação da validade para o padrão dd/mm/aaaa quando terminar de digitar.
-2. Todo vez que salvar o produto, os campos são limpos.
-3. Se clicar fora de qualquer campo, ele perde o foco.
+1. Criação de uma tabela no Excel
+2. Verificações de validade
+'''
+
+'''
+IDEIAS PARA MELHORIAS:
+
+1. Criar uma área de remoção de produtos.
+2. Criar uma área de edição de produtos.
+    2.1. Edição de tipos de produtos.
+    2.2. Edição das observações.
+3. Criar uma área de pesquisa de produtos.
+4. Criar área de login para conectar ao banco de dados.
+5. Criar uma área de relatórios.
+6. Recomendar nome de produtos e fornecedores baseados no que já existe.
 '''
 
 import customtkinter as ctk
@@ -12,9 +24,9 @@ from tkinter import *
 from tkinter import messagebox
 import openpyxl as xl
 import pathlib
-from openpyxl import Workbook
-from openpyxl import load_workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
 import psycopg2
+from datetime import datetime
 
 # Aparência do sistema
 ctk.set_appearance_mode("System")
@@ -55,7 +67,7 @@ class App(ctk.CTk):
         if ficheiro.exists():
             pass
         else:
-            workbook = Workbook()
+            workbook = xl.Workbook()
             folha = workbook.active
             folha['A1'] = "Nome do Produto"
             folha['B1'] = "Código de Barras"
@@ -63,6 +75,27 @@ class App(ctk.CTk):
             folha['D1'] = "Fornecedor"
             folha['E1'] = "Categoria"
             folha['F1'] = "Observações"
+            
+            #Definindo a largura das colunas e deixando em negrito o cabeçalho
+            for col in range(1, folha.max_column + 1):
+                coluna = xl.utils.get_column_letter(col)
+                folha.column_dimensions[coluna].width = 20
+                folha.cell(row=1, column=col).font = xl.styles.Font(bold=True)
+            
+            #Criando a tabela
+            tab = Table(displayName="TabelaProdutos", ref=f"A1:F{folha.max_row}")
+            
+            #Estilo da tabela
+            estilo = TableStyleInfo(
+                name="TableStyleMedium9",
+                showFirstColumn=False,
+                showLastColumn=False,
+                showRowStripes=True,
+                showColumnStripes=False
+            )
+            
+            tab.tableStyleInfo = estilo
+            folha.add_table(tab)
 
             workbook.save("Produtos.xlsx")
         # Função para salvar os dados preenchidos no Excel
@@ -71,6 +104,7 @@ class App(ctk.CTk):
             nome_produto = nome_value.get()
             cod_barras = cod_barras_value.get()
             formatar_validade("")
+            validar_validade(validade_value.get())
             validade = validade_value.get()
             fornecedor = fornecedor_value.get()
             categoria = categoria_combobox.get()
@@ -83,40 +117,41 @@ class App(ctk.CTk):
             else:
                 ficheiro = xl.load_workbook('Produtos.xlsx')
                 folha = ficheiro.active
-                folha.cell(column=1, row=folha.max_row+1, value=nome_produto)
-                folha.cell(column=2, row=folha.max_row, value=cod_barras)
-                folha.cell(column=3, row=folha.max_row, value=validade)
-                folha.cell(column=4, row=folha.max_row, value=fornecedor)
-                folha.cell(column=5, row=folha.max_row, value=categoria)
-                folha.cell(column=6, row=folha.max_row, value=obs)
+                
+                # Adiciona os dados na próxima linha disponível
+                folha.append([nome_produto, cod_barras, validade, fornecedor, categoria, obs])
 
+                #atualiza a tabela
+                tab = folha.tables['TabelaProdutos']
+                tab.ref = f"A1:F{folha.max_row}"
+                
                 ficheiro.save(r"Produtos.xlsx")
                 messagebox.showinfo("Sistema", "Dados salvos com sucesso")
                 
                 clear() #Limpa os campos após salvar
                 
-            ########## ESSA PARTE CONECTA AO PGADMIN  #############
-            try: 
-                conexao = psycopg2.connect(
-                    host="localhost",
-                    database="produtos_db", #COLOQUE O NOME DO SEU BANCO DE DADOS
-                    user="postgres",
-                    password="0L0k1nh0_123!", #COLOQUE A SENHA DO SEU BANCO DE DADOS
-                    port="5432"
-                )
-                cursor = conexao.cursor()
+                ########## ESSA PARTE CONECTA AO PGADMIN  #############
+                try: 
+                    conexao = psycopg2.connect(
+                        host="localhost",
+                        database="produtos_db", #COLOQUE O NOME DO SEU BANCO DE DADOS
+                        user="postgres",
+                        password="0L0k1nh0_123!", #COLOQUE A SENHA DO SEU BANCO DE DADOS
+                        port="5432"
+                    )
+                    cursor = conexao.cursor()
 
-                #inserindo as informações na tabela
-                cursor.execute("""INSERT INTO produtos (nome_produto, cod_barras, validade, fornecedor, categoria, observacoes)VALUES (%s, %s, %s, %s, %s, %s)""", (nome_produto, cod_barras, validade, fornecedor, categoria, obs))
+                    #inserindo as informações na tabela
+                    cursor.execute("""INSERT INTO produtos (nome_produto, cod_barras, validade, fornecedor, categoria, observacoes)VALUES (%s, %s, %s, %s, %s, %s)""", (nome_produto, cod_barras, validade, fornecedor, categoria, obs))
 
-                conexao.commit()
-                cursor.close()
-                conexao.close()
+                    conexao.commit()
+                    cursor.close()
+                    conexao.close()
 
-            except Exception as erro:
-                messagebox.showerror("Erro no banco de dados", f"Erro ao inserir no PostgreSQL:\n{erro}")
+                except Exception as erro:
+                    messagebox.showerror("Erro no banco de dados", f"Erro ao inserir no PostgreSQL:\n{erro}")
 
-            ##############################################################################################
+                ##############################################################################################
         
         # Função para limpar todos os campos do formulário
         def clear():
@@ -139,12 +174,42 @@ class App(ctk.CTk):
         fornecedor_entry = ctk.CTkEntry(self, width=200, textvariable=fornecedor_value, font=("Century Gothic bold", 16), fg_color="transparent")
 
         #Formata a validade para o padrão dd/mm/aaaa
-        def formatar_validade (event): #o "event" recebe o evento "<KeyRelease>" toda vez que o usuário digita
+        def formatar_validade (event): #"event" recebe o evento "<KeyRelease>" toda vez que o usuário digita
             validade = validade_value.get()
             
             if len(validade) == 8 and validade.isdigit(): 
                 validade_value.set(f"{validade[:2]}/{validade[2:4]}/{validade[4:]}")
                 validade_entry.icursor(len(validade_value.get()))  # Move o cursor para o final do texto
+                
+        #verifica se a validade está correta e se o produto não está vencido
+        def validar_validade(valida):
+            #Verifica se foi preenchido totalmente o campo validade
+            try:
+                dia = int(valida[0:2])
+                mes = int(valida[3:5])
+                ano = valida[6:10]
+            except ValueError:
+                messagebox.showerror("Sistema", "Erro!\nFormato de validade inválido.\nUse o formato dd/mm/aaaa")
+                validade_value.set("")
+                return
+            
+            data_atual = datetime.now()
+            
+            # Verifica se o campo validade está no formato correto
+            if len(valida) != 10:
+                messagebox.showerror("Sistema", "Erro!\nFormato de validade inválido.\nUse o formato dd/mm/aaaa")
+                validade_value.set("")
+            
+            # Verifica se o dia, mês e ano são válidos
+            elif dia > 31 or (mes > 12 or mes <= 0):
+                messagebox.showerror("Sistema", "Erro!\nData inválida.\nVerifique o dia, mês e ano")
+                validade_value.set("")
+                
+            #Verifica se o produto está vencido
+            elif datetime.strptime(valida, "%d/%m/%Y") < data_atual:
+                messagebox.showerror("Sistema", "Produto vencido!\nVerifique a validade do produto")
+                validade_value.set("")
+                
         
         #Se sair do compo validade, formata a validade
         validade_entry.bind("<KeyRelease>", formatar_validade)
